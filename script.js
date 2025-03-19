55d424cbb06410341526955535d27eb374c957bd
spiderLambo <spiderLambo.dev@outlook.com>
2025-03-19 21:46:41 +0100

    cam sound play

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const allLi = document.querySelectorAll("li");
 const body = document.body;
+let camSound = new Audio("./sound/openCamera.mp3");
 
 allLi.forEach((li) => {
+  li.addEventListener("click", () => {
+    camSound.play();
+  });
+
   li.addEventListener("mouseenter", () => {
     body.classList.add("hovered");
   });
