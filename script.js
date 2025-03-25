c4f9e34d9b59b769163eb2cd6da2777792d90153
spiderLambo <spiderLambo.dev@outlook.com>
2025-03-25 21:17:07 +0100

    navbar transition

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const allTitles = document.querySelectorAll(".navbarTitle");
 const body = document.body;
+const animatronicsBar = document.querySelector(".navbarTitle + ul");
 let camSound = new Audio("./sound/openCamera.mp3");
 
 allTitles.forEach((title) => {
   title.addEventListener("click", () => {
+    animatronicsBar.style.opacity = 1;
+    animatronicsBar.style.translate = "0 0";
     camSound.play();
   });
 
